55828f3c8dd3909035353f38908c7d77bb47c152
Alexey Kolch <kolychenkov94@mail.ru>
2022-12-04 22:21:12 +0400

    Add ProfileView

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Алексей Колыченков on 04.12.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var nameUser: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var text: UITextView!
+}
